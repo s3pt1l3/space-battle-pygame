b15e5b3db6db9d34fe6b043a12c5ac344652cf9a
Sept1le <powerline.studio@gmail.com>
2021-01-29 23:52:17 +0300

    Created main game algorithm

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
+from pygame.sprite import Group
 
 from stats import GameStats
 from settings import Settings
 from button import Button
 from ship import Ship
 from scoreboard import Scoreboard
+import game_functions as gf
 
 def run():
     """
@@ -24,15 +26,27 @@ def run():
     stats = GameStats(ai_settings)
     sb = Scoreboard(ai_settings, screen, stats)
     
-    # Создание корабля
+    # Создание корабля, пришельцев и пуль
     ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+    
+    # Создание инопланетного флота
+    gf.create_fleet(ai_settings, screen, ship, aliens)
     
     pygame.display.flip()
-    
-    # Проверка на выход из игры
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    pygame.quit()
+    # Главный игровой цикл
+    while True:
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship,
+                        aliens, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens,
+                              bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens,
+                             bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens,
+                         bullets, play_button)
 
 if __name__ == '__main__':
     run()
